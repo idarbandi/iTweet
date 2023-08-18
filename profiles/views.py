13ce86507030497b77ab8642a63b508e4fff84d5
idarbandi <darbandidr99@gmail.com>
2023-08-18 13:55:02 +0330

    going to set signal to create profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Profile
 
-def profile_detail_view(request, *args, **kwargs):
+def profile_detail_view(request, username, *args, **kwargs):
+    qs = Profile.objects.filter(user__username=username)
+    if not qs.exists():
+        raise Http404
     return render(request, 'profiles/detail.html', {})
