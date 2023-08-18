66105b0c3e0b974c6e20d28a8465de21b4f60e8b
idarbandi <darbandidr99@gmail.com>
2023-08-18 17:43:12 +0330

    2 seperate models with one form

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
 
 user = settings.AUTH_USER_MODEL
 
@@ -7,3 +8,11 @@ class Profile(models.Model):
     user = models.OneToOneField(user, on_delete=models.CASCADE)
     location = models.CharField(max_length=220, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
+    
+    
+def user_did_save(sender, instance, created, *args, **kwargs):
+    Profile.objects.get_or_create(user=instance)
+    if created:
+        profile = Profile.objects.get_or_create(user=instance)
+    
+post_save.connect(user_did_save, sender=user)
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import render, redirect
+
 from .models import Profile
+from .forms import ProfileForm
 
 def profile_detail_view(request, username, *args, **kwargs):
     qs = Profile.objects.filter(user__username=username)
     if not qs.exists():
         raise Http404
     return render(request, 'profiles/detail.html', {})
+
+
+def profile_update_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect('/login?next=/profile/update')
+    user = request.user
+    my_profile = user.profile
+    form = ProfileForm(request.POST or None, instance=my_profile)
+    if form.is_valid():
+        profile_obj = form.save(commit=False)
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email_address = form.cleaned_data.get('email_address')
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email_address = email_address
+        user.save()
+        profile_obj.save()
+    context = {
+        'form': form,
+        'btn_label': "Save",
+        'title': 'Update Profile'
+    }
+    return render(request, 'profiles/form.html', context)
